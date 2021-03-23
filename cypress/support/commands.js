a019b471d62a873963309d29801378bf1219a69b
dpgraham <dpgraham.85@gmail.com>
2021-03-23 09:27:02 -0700

    base project

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,3 +25,25 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 //Cypress.Commands.
+
+
+Cypress.Commands.add("login", function (username="standard_user", password="secret_sauce") {
+    cy.get('#user-name').type(username);
+    cy.get('#password').type(password);
+    cy.get('#login-button').click();
+});
+
+// select an inventory item that contains the provided text
+Cypress.Commands.add("selectInventoryItem", function (text) {
+    cy.get(`.inventory_item:contains("${text}") .btn_inventory`).click();
+});
+
+Cypress.Commands.add("checkOut", function (username="Roger", lastName="Wilco", zipCode="12345") {
+    cy.get('#shopping_cart_container a').click();
+    cy.get('#cart_contents_container .cart_item').should('have.length', 1);
+    cy.get('.checkout_button').click();
+    cy.get('#first-name').type(username);
+    cy.get('#last-name').type(lastName);
+    cy.get('#postal-code').type(zipCode);
+    cy.get('.checkout_buttons .btn_primary').click();
+});
